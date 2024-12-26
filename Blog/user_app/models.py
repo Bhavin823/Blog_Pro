e3e4a9e04ee322baab0f4a28a8a7f721df01fa50
Bhavin Chavada <bhavinchavada242846@gmail.com>
2024-12-26 21:06:47 +0530

    make some changes in profile modes

--- Blog/user_app/models.py
+++ Blog/user_app/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfileModel(models.Model):
     user  = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
-    firstname = models.CharField(max_length=50, null=True, blank=True)
-    lastname = models.CharField(max_length=50, null=True, blank=True)
+    firstname = models.CharField(max_length=50,blank=True)
+    lastname = models.CharField(max_length=50,blank=True)
     GENDER = (
         ('M','Male'),
         ('F','Female'),
@@ -14,7 +14,7 @@ class UserProfileModel(models.Model):
     gender = models.CharField(choices=GENDER,max_length=200)
     email = models.EmailField()
     mobile = models.CharField(max_length=10)
-    profile_image = models.ImageField(upload_to='Profile_image',null=True, blank=True)
+    profile_image = models.ImageField(upload_to='Profile_image', blank=True)
 
     def __str__(self):
         return self.user.username
