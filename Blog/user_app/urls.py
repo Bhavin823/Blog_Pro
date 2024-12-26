e4a89592eed3b1b19f050169bed0952142f2a016
Bhavin Chavada <bhavinchavada242846@gmail.com>
2024-12-26 21:08:30 +0530

    add new path relted new views in profile releted

--- Blog/user_app/urls.py
+++ Blog/user_app/urls.py
@@ -4,5 +4,15 @@ from . import views
 
 urlpatterns = [
     path('profile',views.profileView, name='profile'),
-    path('profile_image_upload/', views.profile_image_upload, name='profile_image_upload')
+    
+    # profile_image
+    path('profile_image_upload/', views.profile_image_upload, name='profile_image_upload'),
+
+    path('update_personal_info/', views.update_personal_info, name='update_personal_info'),
+
+    # update email
+    path('update_email/', views.update_email,name='update_email'),
+
+    # update contact
+    path('update_contact/', views.update_contact, name='update_contact'),
 ]
